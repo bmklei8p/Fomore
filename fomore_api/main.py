c2e7d1f5ecd64ac2b5e9c246278e387a5c35b9f0
Alper Ademoglu <alperademoglu@hotmail.com>
2022-11-18 14:00:30 -0500

    Yelp event search API is functional

--- fomore_api/main.py
+++ fomore_api/main.py
@@ -7,6 +7,7 @@ from routers import accounts
 from routers import itineraries
 from routers import sockets
 from routers import events
+from routers import yelp
 
 app = FastAPI()
 
@@ -28,3 +29,4 @@ app.include_router(accounts.router)
 app.include_router(sockets.router)
 app.include_router(events.router, prefix="/api")
 app.include_router(itineraries.router, prefix="/api")
+app.include_router(yelp.router, prefix="/api")
