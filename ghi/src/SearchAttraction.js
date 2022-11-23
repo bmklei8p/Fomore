4ecb6dd24911f63bf4be688a865217b102036494
Connor Swanson <swanscon@gmail.com>
2022-11-23 11:30:28 -0600

    Changed formatting of restaurant cards

--- ghi/src/SearchAttraction.js
+++ ghi/src/SearchAttraction.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -20,7 +19,6 @@ class AttractionList extends React.Component {
       const response = await fetch(url);
       if (response.ok) {
         const data = await response.json();
-        console.log(data)
         this.setState({ attractions: data });
       }
     } catch (e) {
--- ghi/src/SearchEvent.js
+++ ghi/src/SearchEvent.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -20,7 +19,6 @@ class EventList extends React.Component {
       const response = await fetch(url);
       if (response.ok) {
         const data = await response.json();
-        console.log(data)
         this.setState({ events: data });
       }
     } catch (e) {
