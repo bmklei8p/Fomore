4ced67f5ec73d5fd396475048523b8c15faa650c
Bryan Kleinberg <bmklei8p@gmail.com.here>
2022-11-28 21:21:21 -0500

    fixed key errors on attractions, events, restaurants. misc eventForm endpoints

--- ghi/src/EventForm.js
+++ ghi/src/EventForm.js
@@ -1,77 +1,103 @@
 import React from "react";
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import { Container, Row, Col, Card, InputGroup } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { useState } from "react";
+import { Container, Row, Col, Card } from "react-bootstrap";
 import ItineraryList from "./Itinerary";
+import { useGetItinerariesQuery } from "./app/itineraryApi";
+import { useAddEventMutation } from "./app/eventApi";
 
 function CreateEvent() {
+  const [addEvent, { data }] = useAddEventMutation();
+  const body = useGetItinerariesQuery();
+
+  if (body.isLoading) {
+    return <progress className="progress is-primary" max="100"></progress>;
+  }
+
+  const itineraries = body.data.itineraries;
+
   return (
     <div>
-        <Container>
+      <Container>
         <Row>
-            <Col sm={8}>
-                <Card className="item-border" border="light" style={{ width: '40rem' }}>
-                    <Card.Title className="centered">Create a Custom Event</Card.Title>
-                    <Card.Body>
-                        <Row>
-                            <Col sm={4}>
-                                <Form.Label>Select an Itinerary</Form.Label>
-                            </Col>
-                            <Col className="mb-3" sm={8}>
-                                <Form.Select>
-                                    Itinerary
-                                    <option>Itinerary 1</option>
-                                    <option>Itinerary 2</option>
-                                    <option>Itinerary 3</option>
-                                </Form.Select>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <Form.Label>Event Name</Form.Label>
-                            </Col>
-                            <Col className="mb-3" sm={8}>
-                                <Form.Control type="text" placeholder="Enter event name" />
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <Form.Label>Location</Form.Label>
-                            </Col>
-                            <Col className="mb-3" sm={8}>
-                                <Form.Control type="text" label="Location" 
-                                placeholder="Location"/>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <Form.Label>Date</Form.Label>
-                            </Col>
-                            <Col className="mb-3" sm={8}>
-                                <Form.Control type="date" placeholder="Date" />
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <Form.Label>Description</Form.Label>
-                            </Col>
-                            <Col className="mb-3" sm={8}>
-                                <Form.Control as="textarea" label="Description" />
-                            </Col>
-                        </Row>
-                            <Button variant="outline-success" type="submit">
-                                Add to Itinerary
-                            </Button>
-                    </Card.Body>
-                </Card>
-            </Col>
-            <Col sm={4}>
+          <Col sm={8}>
+            <Card
+              className="item-border"
+              border="light"
+              style={{ width: "40rem" }}
+            >
+              <Card.Title className="centered">
+                Create a Custom Event
+              </Card.Title>
+              <Card.Body>
+                <Row>
+                  <Col sm={4}>
+                    <Form.Label>Select an Itinerary</Form.Label>
+                  </Col>
+                  <Col className="mb-3" sm={8}>
+                    <Form.Select>
+                      Itinerary
+                      <option>itineraries</option>
+                      {itineraries.map((itinerary) => {
+                        return (
+                          <option key={itinerary.id} value={itinerary.id}>
+                            {itinerary.name}
+                          </option>
+                        );
+                      })}
+                    </Form.Select>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <Form.Label>Event Name</Form.Label>
+                  </Col>
+                  <Col className="mb-3" sm={8}>
+                    <Form.Control type="text" placeholder="Enter event name" />
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <Form.Label>Location</Form.Label>
+                  </Col>
+                  <Col className="mb-3" sm={8}>
+                    <Form.Control
+                      type="text"
+                      label="Location"
+                      placeholder="Location"
+                    />
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <Form.Label>Date</Form.Label>
+                  </Col>
+                  <Col className="mb-3" sm={8}>
+                    <Form.Control type="date" placeholder="Date" />
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <Form.Label>Description</Form.Label>
+                  </Col>
+                  <Col className="mb-3" sm={8}>
+                    <Form.Control as="textarea" label="Description" />
+                  </Col>
+                </Row>
+                <Button variant="outline-success" type="submit">
+                  Add to Itinerary
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col sm={4}>
             <div className="section-border">
-                <ItineraryList/>
+              <ItineraryList />
             </div>
-            </Col>
+          </Col>
         </Row>
-        </Container>
+      </Container>
     </div>
   );
 }
