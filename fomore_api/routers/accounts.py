9230175e153fef726968fb963830a0821417ad7d
Alper Ademoglu <alperademoglu@hotmail.com>
2022-12-03 21:15:02 -0500

    testing render deployment

--- fomore_api/routers/accounts.py
+++ fomore_api/routers/accounts.py
@@ -45,6 +45,7 @@ not_authorized = HTTPException(
 @router.get("/token", response_model=AccountToken | None)
 async def get_token(
     request: Request,
+    response: Response,
     account: dict = Depends(authenticator.try_get_current_account_data)
 ) -> AccountToken | None:
     if account and authenticator.cookie_name in request.cookies:
@@ -53,6 +54,8 @@ async def get_token(
             "type": "Bearer",
             "account": account,
         }
+    response.headers["Access-Control-Allow-Origin"]: "https://patcerutti23.gitlab.io"
+
 
 
 @router.post("/api/accounts", response_model=AccountToken | HttpError)
