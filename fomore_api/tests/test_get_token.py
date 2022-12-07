2326d042feaf43b2685f9e108694e6230b24a881
Bryan Kleinberg <bmklei8p@gmail.com>
2022-12-07 15:26:12 -0500

    flake8 continued...

--- fomore_api/tests/test_get_token.py
+++ fomore_api/tests/test_get_token.py
@@ -6,7 +6,6 @@ from main import app
 
 client = TestClient(app)
 
-
 def test_get_token_returns_none_for_user_not_logged_in():
     account = {
         "id": "123",
@@ -17,8 +16,10 @@ def test_get_token_returns_none_for_user_not_logged_in():
     app.dependency_overrides[
         authenticator.try_get_current_account_data
     ] = lambda: account
-    response = client.get("/token",
-        cookies={authenticator.cookie_name: "HELLO!"})
+    response = client.get(
+        "/token",
+        cookies={authenticator.cookie_name: "HELLO!"}
+        )
     app.dependency_overrides = {}
     assert response.status_code == 200
     data = response.json()
