e44e29bd9ee71808f25b27ea77a3798ae9da2174
bryan <bmklei8p@gmail.com>
2023-08-02 14:47:49 -0400

    udpated welcome page

--- ghi/src/Pages/LandingPage/Welcome.js
+++ ghi/src/Pages/LandingPage/Welcome.js
@@ -1,30 +1,35 @@
-import logo from "../../media/fomore-dark.png"
-
+import logo from "../../media/fomore-dark.png";
 
 function Welcome() {
-    return (
-        <div className="Welcome-content">
-            <br />
-            <br />
-            {/* <img
-                alt="Fomore title"
-                src={logo}
-                className="welcome-title"
-            ></img> */}
-            <h1 style={{fontSize: '4rem'}}>Fomore</h1>
-            <p>Welcome to FOMORE! The quickest way manage your itineraries.</p>
-            <p>
-                To get started, sign up or log in, and create your first itinerary.
-            </p>
-            <p>
-                Search events or create your own, and add them to your itinerary.
-            </p>
-            <div>
-                <button style={{backgroundColor: "darkgreen"}} className="btn btn-primarygit  btn-lg">Explore as guest</button>
-                <button style={{backgroundColor: "darkgreen"}} className="btn btn-primary btn-lg">Sign up</button>
-            </div>
-        </div>
-    )
+  return (
+    <div
+      style={{
+        paddingLeft: "1rem",
+        paddingRight: "1rem",
+        marginInline: "auto",
+      }}
+      className="Welcome-content"
+    >
+      <h1 style={{ fontSize: "4rem", marginTop: "2rem" }}>Fomore</h1>
+      <p>Welcome to FOMORE! The quickest way manage your itineraries.</p>
+      <p>To get started, sign up or log in, and create your first itinerary.</p>
+      <p>Search events or create your own, and add them to your itinerary.</p>
+      <div style={{ display: "flex", gap: "1rem" }}>
+        <button
+          style={{ backgroundColor: "darkgreen" }}
+          className="btn btn-primary  btn-lg"
+        >
+          Explore as guest
+        </button>
+        <button
+          style={{ backgroundColor: "darkgreen" }}
+          className="btn btn-primary btn-lg"
+        >
+          Sign up
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Welcome;
